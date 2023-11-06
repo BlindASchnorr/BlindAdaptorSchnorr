b99bf4a567016f84939977bb939f9fd7a115c954
XianruiQin <qxrmath@gmail.com>
2023-11-06 19:30:00 +0800

    add comment

--- src/main.rs
+++ src/main.rs
@@ -42,59 +42,59 @@ pub enum AppError {
 }
 
 
-#[test]
-fn verify() {
-    let Generator_jubjub = &*B8;
-    let mut rng = rand::thread_rng();
-    let mask = (BigInt::from(1) << 253) - 1;
-    let adaptor_witness = (rng.gen_bigint(254)& &mask).mod_floor(&Q);
-    println!("adaptor_witness: {:?}", adaptor_witness);
-    let adaptor_statement = Generator_jubjub.mul_scalar(&adaptor_witness);
-    println!("adaptor_statement: {:?}", adaptor_statement);
-    let r = (rng.gen_bigint(254)& &mask).mod_floor(&Q);
-    println!("r: {:?}", r);
-    let R = Generator_jubjub.mul_scalar(&r);
-    println!("R: {:?}", R);
-    let r_plus_as = (r.clone()+adaptor_witness.clone()).mod_floor(&Q);
-    println!("r_plus_as: {:?}", r_plus_as);
-    let R_plus_as = Generator_jubjub.mul_scalar(&r_plus_as);
-    println!("R_plus_as: {:?}", R_plus_as);
-    let R_projective = R.projective();
-    let m = rng.gen_bigint(10)& &mask;
-    let x = (rng.gen_bigint(254)& &mask).mod_floor(&Q);
-    println!("x: {:?}", x);
-    let X = Generator_jubjub.mul_scalar(&x);
-    let alpha = (rng.gen_bigint(254)& &mask).mod_floor(&Q);
-    let alpha_G = Generator_jubjub.mul_scalar(&alpha);
-    let alpha_G_projective = alpha_G.projective();
-    let beta = (rng.gen_bigint(254)& &mask).mod_floor(&Q);
-    let beta_X = X.mul_scalar(&beta.clone());
-    let beta_X_projective = beta_X.projective();
-    let adaptor_statement_projective = adaptor_statement.projective();
-    let R_plus_as_projective = R_projective.add(&adaptor_statement_projective);
-    let R_plus_as = R_plus_as_projective.affine();
-    println!("R_plus_as: {:?}", R_plus_as);
-    let R_prime_projective = alpha_G_projective.add(&beta_X_projective).add(&R_projective);
-    let R_prime = R_prime_projective.affine();
-    let hash_value = schnorr_hash(&X, m.clone(), &R_prime).expect("fail to schnorr_hash");
-    let challenge = (hash_value.clone() + beta).mod_floor(&Q);
-    println!("challenge: {:?}", challenge);
-    let cX = X.mul_scalar(&challenge.clone());
-    //let cx = (challenge.clone()*x.clone()).mod_floor(&Q);
-    let cx = challenge.clone()*x.clone();
-    let cx_G = Generator_jubjub.mul_scalar(&cx);
-    println!("cx_G: {:?}", cx_G);
-    let cX_projective = cX.projective();
-    let cX = cX_projective.affine();
-    println!("cX: {:?}", cX);
-    let R_plus_as_plus_cX_projective = R_plus_as_projective.add(&cX_projective);
-    let R_plus_as_plus_cX = R_plus_as_plus_cX_projective.affine();
-    println!("R_plus_as_plus_cX: {:?}", R_plus_as_plus_cX);
-    let s_prime = (r+adaptor_witness+challenge.clone()*x);
-    let s_prime_G = Generator_jubjub.mul_scalar(&s_prime);
-    println!("s_prime_G: {:?}", s_prime_G);
-    let signature = User::verify(&s_prime, &alpha, &R, R_prime, &adaptor_statement, &challenge, &X);
-}
+// #[test]
+// fn verify() {
+//     let Generator_jubjub = &*B8;
+//     let mut rng = rand::thread_rng();
+//     let mask = (BigInt::from(1) << 253) - 1;
+//     let adaptor_witness = (rng.gen_bigint(254)& &mask).mod_floor(&Q);
+//     println!("adaptor_witness: {:?}", adaptor_witness);
+//     let adaptor_statement = Generator_jubjub.mul_scalar(&adaptor_witness);
+//     println!("adaptor_statement: {:?}", adaptor_statement);
+//     let r = (rng.gen_bigint(254)& &mask).mod_floor(&Q);
+//     println!("r: {:?}", r);
+//     let R = Generator_jubjub.mul_scalar(&r);
+//     println!("R: {:?}", R);
+//     let r_plus_as = (r.clone()+adaptor_witness.clone()).mod_floor(&Q);
+//     println!("r_plus_as: {:?}", r_plus_as);
+//     let R_plus_as = Generator_jubjub.mul_scalar(&r_plus_as);
+//     println!("R_plus_as: {:?}", R_plus_as);
+//     let R_projective = R.projective();
+//     let m = rng.gen_bigint(10)& &mask;
+//     let x = (rng.gen_bigint(254)& &mask).mod_floor(&Q);
+//     println!("x: {:?}", x);
+//     let X = Generator_jubjub.mul_scalar(&x);
+//     let alpha = (rng.gen_bigint(254)& &mask).mod_floor(&Q);
+//     let alpha_G = Generator_jubjub.mul_scalar(&alpha);
+//     let alpha_G_projective = alpha_G.projective();
+//     let beta = (rng.gen_bigint(254)& &mask).mod_floor(&Q);
+//     let beta_X = X.mul_scalar(&beta.clone());
+//     let beta_X_projective = beta_X.projective();
+//     let adaptor_statement_projective = adaptor_statement.projective();
+//     let R_plus_as_projective = R_projective.add(&adaptor_statement_projective);
+//     let R_plus_as = R_plus_as_projective.affine();
+//     println!("R_plus_as: {:?}", R_plus_as);
+//     let R_prime_projective = alpha_G_projective.add(&beta_X_projective).add(&R_projective);
+//     let R_prime = R_prime_projective.affine();
+//     let hash_value = schnorr_hash(&X, m.clone(), &R_prime).expect("fail to schnorr_hash");
+//     let challenge = (hash_value.clone() + beta).mod_floor(&Q);
+//     println!("challenge: {:?}", challenge);
+//     let cX = X.mul_scalar(&challenge.clone());
+//     //let cx = (challenge.clone()*x.clone()).mod_floor(&Q);
+//     let cx = challenge.clone()*x.clone();
+//     let cx_G = Generator_jubjub.mul_scalar(&cx);
+//     println!("cx_G: {:?}", cx_G);
+//     let cX_projective = cX.projective();
+//     let cX = cX_projective.affine();
+//     println!("cX: {:?}", cX);
+//     let R_plus_as_plus_cX_projective = R_plus_as_projective.add(&cX_projective);
+//     let R_plus_as_plus_cX = R_plus_as_plus_cX_projective.affine();
+//     println!("R_plus_as_plus_cX: {:?}", R_plus_as_plus_cX);
+//     let s_prime = (r+adaptor_witness+challenge.clone()*x);
+//     let s_prime_G = Generator_jubjub.mul_scalar(&s_prime);
+//     println!("s_prime_G: {:?}", s_prime_G);
+//     let signature = User::verify(&s_prime, &alpha, &R, R_prime, &adaptor_statement, &challenge, &X);
+// }
 
 
 fn main()-> Result<()> {
@@ -108,18 +108,31 @@ fn main()-> Result<()> {
     let duration = start.elapsed();
     println!("Time elapsed to transform r1cs circuit is: {:?}", duration);
     let start = Instant::now();
+    //Then we generate the parameters
     let Generator_jubjub = &*B8;
     let mut rng = rand::thread_rng();
+
+    //generate the secret key and public key
+    let x = rng.gen_bigint(254)& &mask;
+    let X = Generator_jubjub.mul_scalar(&x);
+
+    //prepare the adaptor statement 
     let adaptor_witness = rng.gen_bigint(254);
     let adaptor_statement = Generator_jubjub.mul_scalar(&adaptor_witness);
     let mask = (BigInt::from(1) << 253) - 1;
+
+    //message
     let m = rng.gen_bigint(10)& &mask;
-    let x = rng.gen_bigint(254)& &mask;
-    let X = Generator_jubjub.mul_scalar(&x);
+
+    //the first step of the protocol: compute the ciphertext
     let user_output_1 = User::sent_1(&m); 
+
+    //the second step of the protocol: compute the challenge group element
     let signer_output = signer::Signer::response_1(&user_output_1.ciphertext);
     let r = signer_output.signer_state.r;
     let R = signer_output.R;
+
+    //the third step of the protocol: compute the proof
     let alpha = user_output_1.user_state.alpha;
     let beta = user_output_1.user_state.beta;
     let rho = user_output_1.user_state.rho;
@@ -129,9 +142,13 @@ fn main()-> Result<()> {
     let inputs = user_output_2.inputs;
     let params = user_output_2.params;
     let proof = &user_output_2.proof;
+
+    //the fourth step of the protocol: compute the blind adaptor schnorr signature
     let signer_response = Signer::response_2(&x, &challenge, params, inputs, proof, &r, &adaptor_witness).unwrap();
     let s_prime = &signer_response;
     let R_prime = user_output_2.R_prime;
+
+    //the fifth step of the protocol: verify and output the adaptor schnorr signature
     let signature = User::verify(s_prime, &alpha, &R, R_prime, &adaptor_statement, &challenge, &X);
     let duration = start.elapsed();
     println!("Time elapsed completing blind adaptor signature is: {:?}", duration);
